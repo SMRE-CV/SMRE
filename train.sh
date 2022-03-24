7c4b6858e9afc1fbf496a9f0a2acf3556ae83a7f
SMRE-CV <96560976+SMRE-CV@users.noreply.github.com>
2022-03-24 10:49:18 +0800

    Update train.sh

--- train.sh
+++ train.sh
@@ -2,7 +2,6 @@ CUDA_VISIBLE_DEVICES=1 python train.py --dataset=msvd --model=RMN \
  --result_dir=results/msvd_best --use_lin_loss \
  --learning_rate_decay --learning_rate_decay_every=5 \
  --learning_rate_decay_rate=3 \
- --use_loc --use_rel --use_func \
  --learning_rate=1e-4 --attention=soft \
  --choose=sota \
  --data_aug=True \
